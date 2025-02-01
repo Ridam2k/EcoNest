3c6b666fb1f2e309feee7ee6ca435a4ea2218406
Srivastava, Ridam <ridam2k@gmail.com>
2025-02-01 12:12:56 -0500

    lcoal

--- api/carbon_footprint_analyzer.py
+++ api/carbon_footprint_analyzer.py
@@ -4,11 +4,11 @@ from typing import Dict, List, Optional, Union, Any, Tuple
 import ast
 import json
 import re
-import UpdatedStructuredResponse as usr
-import RecommendationsStructuredResponse as rsr
-import AnalysisStructuredResponse as asr
-import SponsoredProductsResponse as psr
-import httpx
+import api.UpdatedStructuredResponse as usr
+import api.RecommendationsStructuredResponse as rsr
+import api.AnalysisStructuredResponse as asr
+import api.SponsoredProductsResponse as psr
+
 
 
 # Example user input
